462e73085064094d01dd37653a767824b997a73d
DashaDudk <dudka1610@gmail.com>
2024-11-18 00:03:15 +0200

    .

---
+++ lex_yacc-parser/tests/test1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+    int x=1;
+    int y=2;
+    int z=3;
+    x=3;
+    y=10;
+    z=5;
+    return 1;
+}
---
+++ lex_yacc-parser/tests/test2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int a;
+    int x=1;
+    int y=2;
+    int z=3;
+    x=3;
+    y=10;
+    z=5;
+    if(x>5) {
+        for(int k=0; k<10; k++) {
+            y = x+3;
+            printf("Hello!");
+        }
+    } else {
+        int idx = 1;
+    }
+    return 1;
+}
